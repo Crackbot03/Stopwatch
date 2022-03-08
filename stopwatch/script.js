39359d597f4252da25587a90b72da644e5935553
Crackbot <85601876+Crackbot03@users.noreply.github.com>
2022-03-08 20:19:19 +0530

    Add files via upload

---
+++ stopwatch/script.js
@@ -0,0 +1,48 @@
+const time_el = document.querySelector('.watch .Time');
+const start_btn = document.getElementById('start');
+const stop_btn = document.getElementById("stop");
+const reset_btn = document.getElementById("reset");
+
+let seconds = 0;
+let interval = null;
+
+//event listeners
+start_btn.addEventListener('click',start);
+stop_btn.addEventListener('click',stop);
+reset_btn.addEventListener('click',reset);
+
+function timer () {
+
+    seconds++;
+
+    //formating time
+    let hrs = Math.floor(seconds / 3600);
+    let mins = Math.floor((seconds - (hrs * 3600)) / 60);
+    let secs =  seconds % 60;
+
+//if (secs < 10) secs = '0' + secs;
+//if (mins < 10) secs = '0' + mins;
+//if (hrs < 10) secs = '0' + hrs;
+
+    time_el.innerText = `${hrs}:${mins}:${secs}`;
+
+}
+function start (){
+    if (interval){
+        return
+    }
+    interval = setInterval(timer,1000);
+}
+
+function stop(){
+    clearInterval(interval);
+    interval = null;
+}
+function reset(){
+    stop();
+    seconds = 0;
+    time_el.innerText = '00:00:00';
+
+}
+
+
